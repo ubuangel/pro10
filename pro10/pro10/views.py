30e2f415509bbf7f330941c55e462e763a42eb62
angel <andresbjar97@gmail.com>
2020-11-03 19:50:35 -0500

    mmm eeror a limportar la vista

--- pro10/pro10/views.py
+++ pro10/pro10/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from _datetime import datetime
 
 def saludo(request):#primer vbuista
     return HttpResponse("<html><body><h1>hola colegas a meter codigo duro y bueno</h1></body></html>")
@@ -6,3 +7,9 @@ def saludo(request):#primer vbuista
 def  despedida(request):
     return HttpResponse("esdta luego angel")
     #//devolver e lonjeto hhtpresponse y como parametro el pasamos 
+def dameFecha(request):
+    fecha_actual=datetime.datetime.now()
+        
+    documento="""<html><bosy><h1> Fecha y hora actuales %s</html></bosy></h1>"---%fecha_actual"""%fecha_actual
+        
+    return HttpResponse(documento)

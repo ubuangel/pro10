60d8d839246c2c98d953026eae78f640dffb29a6
angel <andresbjar97@gmail.com>
2020-11-04 00:34:41 -0500

    comentando en url

--- pro10/pro10/urls.py
+++ pro10/pro10/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+#esta es la rama 2
 from django.contrib import admin
 from django.urls import path
 from pro10.views import saludo,despedida

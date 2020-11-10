9aee5e29b4c59031f777a94c29d2580d75cc70c9
angel <andresbjar97@gmail.com>
2020-11-10 12:25:29 -0500

    modificando

--- pro10/pro10/polls/models.py
+++ pro10/pro10/polls/models.py
@@ -1,14 +1,12 @@
 
 from django.db import models
 
-class Question (models.Model):
-    question_=models.CharField(max_length=200);
-    models.DateTimeField('data published');
-    
-    
-class choice(models.Model):
-    question=models.ForeignKey(Question,on_delete=models.CASCADE)
-                            
-    choice_text = models.CharField(max_length=200)
-   
-    votes = models.IntegerField(default=0)
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text

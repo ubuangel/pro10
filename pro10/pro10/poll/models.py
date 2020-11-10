2d4b3f19d2fc95e416503d469a5f95eaf22381bc
angel <andresbjar97@gmail.com>
2020-11-10 05:53:06 -0500

    corrigiendo ortografia

--- pro10/pro10/poll/models.py
+++ pro10/pro10/poll/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Question (models.Model):
     question_=models.CharField(max_length=200);
-    models.dateTimeField('data published');
+    models.DateTimeField('data published');
     
     
 class choice(models.Model):
-    question=models.ForeigKey(Question,on_delete=models.CASCADE)
+    question=models.ForeignKey(Question,on_delete=models.CASCADE)
                             
      choice_text=models.CharFields(max_length=200)
      models.integerField(default=0)
